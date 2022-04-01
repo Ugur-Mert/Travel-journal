68dc2b5009bacbef332f849bd718ee7484f420e3
Ugur-Mert <u.mert.ozderr@gmail.com>
2022-04-01 19:54:36 +0300

    Last changes

--- src/data.js
+++ src/data.js
@@ -8,32 +8,32 @@ export default [
     startDate: "29 Mar, 2022",
     endDate: "1 Apr, 2022",
     description:
-      "Odunpazarı Evleri, Eskişehir'in ilk yerleşim yerini oluşturan Odunpazarı semtindeki Osmanlı Dönemi'nden kalma tarihi evlerdir. Safranbolu, Beypazarı, Göynük gibi yerlerdeki mimari özellik ve motifleri taşırlar. UNESCO dünya mirası listesindedir.",
+      "Odunpazarı Evleri, Eskişehir'in ilk yerleşim yerini oluşturan Odunpazarı semtindeki Osmanlı Dönemi'nden kalma tarihi evlerdir.",
     imageUrl:
       "https://i.pinimg.com/564x/69/52/12/695212d1e0a8294fb4d67017b606b561.jpg",
   },
   {
     id: 2,
-    title: "Odunpazarı Evleri",
+    title: "Aizanoi Antik Kenti",
     location: "TURKEY",
-    googleMapsUrl: "https://goo.gl/maps/3wU8PTaVi22AwNx6A",
-    startDate: "29 Mar, 2022",
-    endDate: "1 Apr, 2022",
+    googleMapsUrl: "https://goo.gl/maps/9AdBqC3geVAVW3xW8",
+    startDate: "10 Mar, 2022",
+    endDate: "11 Mar, 2022",
     description:
-      "Odunpazarı Evleri, Eskişehir'in ilk yerleşim yerini oluşturan Odunpazarı semtindeki Osmanlı Dönemi'nden kalma tarihi evlerdir. Safranbolu, Beypazarı, Göynük gibi yerlerdeki mimari özellik ve motifleri taşırlar. UNESCO dünya mirası listesindedir.",
+      "Aizanoi kenti, antik Frigya'ya bağlı olarak yaşayan Aizanitis’lerin ana yerleşim merkeziydi.",
     imageUrl:
-      "https://i.pinimg.com/564x/69/52/12/695212d1e0a8294fb4d67017b606b561.jpg",
+      "https://i.pinimg.com/736x/3e/2e/e7/3e2ee7ede806858cd78728e8920d411d.jpg",
   },
   {
     id: 3,
-    title: "Odunpazarı Evleri",
+    title: "Dumlupınar Şehitliği",
     location: "TURKEY",
-    googleMapsUrl: "https://goo.gl/maps/3wU8PTaVi22AwNx6A",
-    startDate: "29 Mar, 2022",
-    endDate: "1 Apr, 2022",
+    googleMapsUrl: "https://g.page/dumlupinarsehitligi?share",
+    startDate: "8 Mar, 2022",
+    endDate: "9 Mar, 2022",
     description:
-      "Odunpazarı Evleri, Eskişehir'in ilk yerleşim yerini oluşturan Odunpazarı semtindeki Osmanlı Dönemi'nden kalma tarihi evlerdir. Safranbolu, Beypazarı, Göynük gibi yerlerdeki mimari özellik ve motifleri taşırlar. UNESCO dünya mirası listesindedir.",
+      "Kütahya Dumlupınar Kurtuluş Savaşı Şehitliği, Dumlupınar Meydan Muharebesi'nde ölen Türk askerleri anısına Kütahya'nın Dumlupınar ilçesinde 1992 yılında Kültür Bakanlığı tarafından yaptırılan şehitliktir. Büyük Taarruz'un 70. yıldönümü olan 30 Ağustos1992 günü hizmete açılmıştır.",
     imageUrl:
-      "https://i.pinimg.com/564x/69/52/12/695212d1e0a8294fb4d67017b606b561.jpg",
+      "https://i.pinimg.com/originals/e3/df/b7/e3dfb780d3c16936e35c9cf6e1b3ecf2.jpg",
   },
 ];
